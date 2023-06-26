8888d81c7056a95da2a4ba5fa0fe05fb660a440a
Martin Fulop <martin999b@gmail.com>
2023-06-26 23:07:38 +0200

    test

--- index.js
+++ index.js
@@ -11,55 +11,55 @@ module.exports = class Ripple {
     offsetX,
     clickPointY,
     elementHeight,
-    offsetY,
+    offsetY
   ) {
     this.x = clickPointX - offsetX > elementWidth / 2 ? 0 : elementWidth;
     this.y = clickPointY - offsetY > elementHeight / 2 ? 0 : elementHeight;
     this.z = Math.hypot(
       this.x - (clickPointX - offsetX),
-      this.y - (clickPointY - offsetY),
+      this.y - (clickPointY - offsetY)
     );
 
     return this.z;
   }
 
   appyStyles(element, color, rect, radius, event) {
-    element.classList.add('ripple');
+    element.classList.add("ripple");
     element.style.backgroundColor =
-      color === 'dark' ? 'rgba(0,0,0, 0.2)' : 'rgba(255,255,255, 0.3)';
-    element.style.borderRadius = '50%';
-    element.style.pointerEvents = 'none';
-    element.style.position = 'absolute';
-    element.style.left = event.clientX - rect.left - radius + 'px';
-    element.style.top = event.clientY - rect.top - radius + 'px';
-    element.style.width = element.style.height = radius * 2 + 'px';
+      color === "dark" ? "rgba(0,0,0, 0.2)" : "rgba(255,255,255, 0.3)";
+    element.style.borderRadius = "50%";
+    element.style.pointerEvents = "none";
+    element.style.position = "absolute";
+    element.style.left = event.clientX - rect.left - radius + "px";
+    element.style.top = event.clientY - rect.top - radius + "px";
+    element.style.width = element.style.height = radius * 2 + "px";
   }
 
   applyAnimation(element) {
     element.animate(
       [
         {
-          transform: 'scale(0)',
+          transform: "scale(0)",
           opacity: 1,
         },
         {
-          transform: 'scale(1.5)',
+          transform: "scale(1.5)",
           opacity: 0,
         },
       ],
       {
         duration: 500,
-        easing: 'linear',
-        fill: 'forwards'
-      },
+        easing: "linear",
+        fill: "forwards",
+      }
     );
   }
 
   create(event, color) {
     const element = event.currentTarget;
 
-    element.style.position = 'relative';
-    element.style.overflow = 'hidden';
+    element.style.position = "relative";
+    element.style.overflow = "hidden";
 
     const rect = element.getBoundingClientRect();
 
@@ -69,16 +69,16 @@ module.exports = class Ripple {
       rect.left,
       event.clientY,
       element.offsetHeight,
-      rect.top,
+      rect.top
     );
 
-    const circle = document.createElement('span');
+    const circle = document.createElement("span");
 
     this.appyStyles(circle, color, rect, radius, event);
     this.applyAnimation(circle);
 
     element.appendChild(circle);
 
-    setTimeout(() => circle.remove(), 500);
+    setTimeout(() => circle.remove(), 5000);
   }
 };
